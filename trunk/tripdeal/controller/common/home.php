4348a239044c584e299647b88b6afcd1e2bd19fd
henrycuongduy@gmail.com <henrycuongduy@gmail.com@04f3c33e-3a50-b635-46d1-97ff18a051a2>
2014-08-04 06:07:18 +0000

    test commit :|

--- trunk/tripdeal/controller/common/home.php
+++ trunk/tripdeal/controller/common/home.php
@@ -1,23 +1,28 @@
-<?php  
+<?php
 class ControllerCommonHome extends Controller {
-	public function index() {
-		$this->document->setTitle($this->config->get('config_title'));
-		$this->document->setDescription($this->config->get('config_meta_description'));
+    public function index() {
+        $this->load->model('catalog/product');
 
-		$this->data['heading_title'] = $this->config->get('config_title');
-		
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/home.tpl')) {
-			$this->template = $this->config->get('config_template') . '/template/common/home.tpl';
-		} else {
-			$this->template = 'default/template/common/home.tpl';
-		}
+        $this->document->setTitle($this->config->get('config_title'));
+        $this->document->setDescription($this->config->get('config_meta_description'));
+
+        $this->data['heading_title'] = $this->config->get('config_title');
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/home.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/common/home.tpl';
+        } else {
+            $this->template = 'default/template/common/home.tpl';
+        }
 
         $this->children = array(
             'common/header',
             'common/footer',
         );
-										
-		$this->response->setOutput($this->render());
-	}
+
+        $dataModel = $this->model_catalog_product->getNewProductListReceive('short', 0 , 20);
+        $this->data['dataModel'] = $dataModel;
+
+        $this->response->setOutput($this->render());
+    }
 }
 ?>
